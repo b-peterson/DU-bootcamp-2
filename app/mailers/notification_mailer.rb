70eb5246bd6e15e8093eeedb4358ab6cade8daba
Benjamin Peterson <petersonbenjamint@gmail.com>
2019-09-12 22:03:35 +0000

    Added places to user page and final touches to the app

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ApplicationMailer
-	default from: "no-reply@nosterapp.com"
+	default from: "no-reply@nomsterapp.com"
 	
 	def comment_added(comment)
 		@place = comment.place
